531f9a8d05a074e2be111a7daa43e00aeaf5fa62
paulamorales089 <paitomorales03@gmail.com>
2023-10-18 22:09:52 -0500

    commiting before pulling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -51,7 +51,7 @@ export default class Header extends Component {
                         <Link to='/user'><img className='header-container-right-div-user-img' src='/user.png'></img></Link>
                     </div>}
                 {this.state.isLoggedIn === false && <div className='header-container-right-div'>
-                    <a href='' className='header-container-right-div-link'>Acceder</a>
+                    <a href='' className='header-container-right-div-link'>Ingresar</a>
                 </div>}
             </div>
         </header>
