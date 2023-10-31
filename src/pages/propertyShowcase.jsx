bf3a043198165f6d594ec300cdae75e0eff072b7
AHTepsilon <alehertoro725@gmail.com>
2023-10-31 16:58:29 -0500

    made progress with implementation of filters

--- src/pages/propertyShowcase.jsx
+++ src/pages/propertyShowcase.jsx
@@ -20,6 +20,12 @@ export default class PropertyShowcase extends Component {
             userPreferences: [],
             listOfProperties: [],
             loading: true,
+            minPriceFilter:'',
+            maxPriceFilter: '',
+            stractFilter: '',
+            ageFilter: '',
+            roomFilter: '',
+            parkingFilter: '',
         }
 
     }
@@ -76,15 +82,25 @@ export default class PropertyShowcase extends Component {
   }
 
   fillDisplaySection = () => {
-
+    return this.state.listOfProperties;
   }
 
   componentDidUpdate(){
     if(this.state.listOfProperties.length > 0){
       setTimeout(() => {
-        this.setState({loading: false})
+        this.setState({loading: false});
+        console.log(this.state.listOfProperties);
       }, 3000);
-    }
+    } 
+  }
+
+  clearArray = () => {
+    this.setState({ listOfProperties: [] });
+    this.forceUpdate();
+  }
+
+  performFilters = () => {
+    let initialArray = this.state.listOfProperties;
   }
   
   componentDidMount = async() => {
@@ -106,11 +122,11 @@ export default class PropertyShowcase extends Component {
                 <div className='propertyShowcase-filtersSection-area-div-1-1-textInputs'>
                   <div className='propertyShowcase-filtersSection-area-div-1-1-textInputs-inner'>
                     <p className='propertyShowcase-filtersSection-area-div-1-1-textInputs-inner-text'>Desde</p>
-                    <input type="text" className='propertyShowcase-filtersSection-area-div-1-1-textInputs-inner-input'/>
+                    <input onChange={(e)=> {this.setState({minPriceFilter: e.target.value})}} type="text" className='propertyShowcase-filtersSection-area-div-1-1-textInputs-inner-input'/>
                   </div>
                   <div className='propertyShowcase-filtersSection-area-div-1-1-textInputs-inner'>
                     <p className='propertyShowcase-filtersSection-area-div-1-1-textInputs-inner-text'>Hasta</p>
-                    <input type="text" className='propertyShowcase-filtersSection-area-div-1-1-textInputs-inner-input'/>
+                    <input onChange={(e)=> {this.setState({maxPriceFilter: e.target.value})}} type="text" className='propertyShowcase-filtersSection-area-div-1-1-textInputs-inner-input'/>
                   </div>
                 </div>
               </div>
@@ -119,13 +135,13 @@ export default class PropertyShowcase extends Component {
               <h3 className='propertyShowcase-filtersSection-area-div-1-title'>Estrato</h3>
               <div className='propertyShowcase-filtersSection-area-div-1-1'>
                 <div className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs'>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Todos</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>1</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>2</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>3</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>4</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>5</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>6</button>
+                  <button onClick={(e)=> {this.setState({stractFilter: ''})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Todos</button>
+                  <button onClick={(e)=> {this.setState({stractFilter: 1})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>1</button>
+                  <button onClick={(e)=> {this.setState({stractFilter: 2})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>2</button>
+                  <button onClick={(e)=> {this.setState({stractFilter: 3})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>3</button>
+                  <button onClick={(e)=> {this.setState({stractFilter: 4})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>4</button>
+                  <button onClick={(e)=> {this.setState({stractFilter: 5})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>5</button>
+                  <button onClick={(e)=> {this.setState({stractFilter: 6})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>6</button>
                 </div>
               </div>
             </div>
@@ -133,10 +149,11 @@ export default class PropertyShowcase extends Component {
               <h3 className='propertyShowcase-filtersSection-area-div-1-title'>Antigüedad del inmueble (en años)</h3>
               <div className='propertyShowcase-filtersSection-area-div-1-1'>
                 <div className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs'>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Entre 0 y 5</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Entre 6 y 10</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Entre 11 y 15</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Más de 16 años</button>
+                <button onClick={(e)=> {this.setState({stractFilter: ''})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Cualquiera</button>
+                  <button onClick={(e)=> {this.setState({stractFilter: 1})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Entre 0 y 5</button>
+                  <button onClick={(e)=> {this.setState({stractFilter: 2})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Entre 6 y 10</button>
+                  <button onClick={(e)=> {this.setState({stractFilter: 3})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Entre 11 y 15</button>
+                  <button onClick={(e)=> {this.setState({stractFilter: 4})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Más de 16 años</button>
                 </div>
               </div>
             </div>
@@ -144,12 +161,13 @@ export default class PropertyShowcase extends Component {
               <h3 className='propertyShowcase-filtersSection-area-div-1-title'>Cantidad de habitaciones</h3>
               <div className='propertyShowcase-filtersSection-area-div-1-1'>
                 <div className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs'>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>1</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>2</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>3</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>4</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>5</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>6+</button>
+                <button onClick={(e)=> {this.setState({roomFilter: ''})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Cualquiera</button>
+                  <button onClick={(e)=> {this.setState({roomFilter: 1})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>1</button>
+                  <button onClick={(e)=> {this.setState({roomFilter: 2})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>2</button>
+                  <button onClick={(e)=> {this.setState({roomFilter: 3})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>3</button>
+                  <button onClick={(e)=> {this.setState({roomFilter: 4})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>4</button>
+                  <button onClick={(e)=> {this.setState({roomFilter: 5})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>5</button>
+                  <button onClick={(e)=> {this.setState({roomFilter: 6})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>6+</button>
                 </div>
               </div>
             </div>
@@ -157,13 +175,15 @@ export default class PropertyShowcase extends Component {
               <h3 className='propertyShowcase-filtersSection-area-div-1-title'>Parqueaderos</h3>
               <div className='propertyShowcase-filtersSection-area-div-1-1'>
                 <div className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs'>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>No tiene</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>1</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>2</button>
-                  <button className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>3+</button>
+                <button onClick={(e)=> {this.setState({parkingFilter: ''})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>Cualquiera</button>
+                  <button onClick={(e)=> {this.setState({parkingFilter: 1})}} className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>No tiene</button>
+                  <button onClick={(e)=> {this.setState({parkingFilter: 2})}}  className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>1</button>
+                  <button onClick={(e)=> {this.setState({parkingFilter: 3})}}  className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>2</button>
+                  <button onClick={(e)=> {this.setState({parkingFilter: 4})}}  className='propertyShowcase-filtersSection-area-div-1-1-buttonInputs-button'>3+</button>
                 </div>
               </div>
             </div>
+            <button onClick={this.clearArray} className='propertyShowcase-filtersSection-area-button'>Aplicar filtros</button>
           </div>
         </section>
         <section className='propertyShowcase-section' id='propertyShowcase-section'>
